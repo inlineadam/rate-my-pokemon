4a081966be9332ce2c0cc796cbf5adcb67c4ed27
Adam Babarczi <inlineadam@gmail.com>
2023-10-30 06:52:07 +0100

    feat(vote): generate pokemonDetails interfaces

--- src/app/shared/models/pokemons.model.ts
+++ src/app/shared/models/pokemons.model.ts
@@ -9,3 +9,206 @@ export interface PokemonNamedAPIResource {
   name: string;
   url: string;
 }
+
+export interface PokemonDetails {
+  id: number;
+  name: string;
+  base_experience: number;
+  height: number;
+  is_default: boolean;
+  order: number;
+  weight: number;
+  abilities: Ability2[];
+  forms: Ability[];
+  game_indices: Gameindex[];
+  held_items: Helditem[];
+  location_area_encounters: string;
+  moves: Move[];
+  species: Ability;
+  sprites: Sprites;
+  stats: Stat[];
+  types: Type[];
+  past_types: Pasttype[];
+}
+
+interface Pasttype {
+  generation: Ability;
+  types: Type[];
+}
+
+export interface Type {
+  slot: number;
+  type: Ability;
+}
+
+interface Stat {
+  base_stat: number;
+  effort: number;
+  stat: Ability;
+}
+
+interface Sprites {
+  back_default: string;
+  back_female?: any;
+  back_shiny: string;
+  back_shiny_female?: any;
+  front_default: string;
+  front_female?: any;
+  front_shiny: string;
+  front_shiny_female?: any;
+  other: Other;
+  versions: Versions;
+}
+
+interface Versions {
+  'generation-i': Generationi;
+  'generation-ii': Generationii;
+  'generation-iii': Generationiii;
+  'generation-iv': Generationiv;
+  'generation-v': Generationv;
+  'generation-vi': Generationvi;
+  'generation-vii': Generationvii;
+  'generation-viii': Generationviii;
+}
+
+interface Generationviii {
+  icons: Dreamworld;
+}
+
+interface Generationvii {
+  icons: Dreamworld;
+  'ultra-sun-ultra-moon': Home;
+}
+
+interface Generationvi {
+  'omegaruby-alphasapphire': Home;
+  'x-y': Home;
+}
+
+interface Generationv {
+  'black-white': Blackwhite;
+}
+
+interface Blackwhite {
+  animated: Diamondpearl;
+  back_default: string;
+  back_female?: any;
+  back_shiny: string;
+  back_shiny_female?: any;
+  front_default: string;
+  front_female?: any;
+  front_shiny: string;
+  front_shiny_female?: any;
+}
+
+interface Generationiv {
+  'diamond-pearl': Diamondpearl;
+  'heartgold-soulsilver': Diamondpearl;
+  platinum: Diamondpearl;
+}
+
+interface Diamondpearl {
+  back_default: string;
+  back_female?: any;
+  back_shiny: string;
+  back_shiny_female?: any;
+  front_default: string;
+  front_female?: any;
+  front_shiny: string;
+  front_shiny_female?: any;
+}
+
+interface Generationiii {
+  emerald: Emerald;
+  'firered-leafgreen': Crystal;
+  'ruby-sapphire': Crystal;
+}
+
+interface Emerald {
+  front_default: string;
+  front_shiny: string;
+}
+
+interface Generationii {
+  crystal: Crystal;
+  gold: Crystal;
+  silver: Crystal;
+}
+
+interface Crystal {
+  back_default: string;
+  back_shiny: string;
+  front_default: string;
+  front_shiny: string;
+}
+
+interface Generationi {
+  'red-blue': Redblue;
+  yellow: Redblue;
+}
+
+interface Redblue {
+  back_default: string;
+  back_gray: string;
+  front_default: string;
+  front_gray: string;
+}
+
+interface Other {
+  dream_world: Dreamworld;
+  home: Home;
+  'official-artwork': Officialartwork;
+}
+
+interface Officialartwork {
+  front_default: string;
+}
+
+interface Home {
+  front_default: string;
+  front_female?: any;
+  front_shiny: string;
+  front_shiny_female?: any;
+}
+
+interface Dreamworld {
+  front_default: string;
+  front_female?: any;
+}
+
+interface Move {
+  move: Ability;
+  version_group_details: Versiongroupdetail[];
+}
+
+interface Versiongroupdetail {
+  level_learned_at: number;
+  version_group: Ability;
+  move_learn_method: Ability;
+}
+
+interface Helditem {
+  item: Ability;
+  version_details: Versiondetail[];
+}
+
+interface Versiondetail {
+  rarity: number;
+  version: Ability;
+}
+
+interface Gameindex {
+  game_index: number;
+  version: Ability;
+}
+
+interface Ability2 {
+  is_hidden: boolean;
+  slot: number;
+  ability: Ability;
+}
+
+interface Ability {
+  name: string;
+  url: string;
+}
