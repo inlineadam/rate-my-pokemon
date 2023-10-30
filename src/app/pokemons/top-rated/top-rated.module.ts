2e06f5ee9eb4d44e0caae00f4785644494566a9f
Adam Babarczi <inlineadam@gmail.com>
2023-10-30 05:58:16 +0100

    feat(top-rated): add top rated module and component

---
+++ src/app/pokemons/top-rated/top-rated.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopRatedComponent } from './top-rated.component';
+
+
+
+@NgModule({
+  declarations: [
+    TopRatedComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TopRatedModule { }
